a2bd8a301667b8af1c876fc65ddfd22a0524c121
The Pygoscelis <charlietech45@gmail.com>
2015-02-08 00:20:55 -0600

    Discrete math script added

---
+++ discreteMaths.rkt
@@ -0,0 +1,24 @@
+#lang racket
+;Successor Function - Domain N Range N
+(define (s n)
+  (if (< n 0)
+      (error 'Domain "Domain Natural Numbers Only")
+      (+ 1 n)))
+;Predessor Function - Domain N 
+(define (Pr n)
+  (if (equal? 0 n)
+      0
+      (- n 1)))
+;Add strips Successors off of B and put them on A
+;until b is zero or has no more successors.
+(define (add a b)
+  (if (equal? 0 b)
+      a
+      (add (s a) (Pr b))))
+;Subtract strips as many successors off a as b has
+;Since domain is limited to N no negatives will
+;be returned.
+(define (subtract a b)
+  (if (equal? 0 b)
+      a
+      (subtract (Pr a) (Pr b))))
